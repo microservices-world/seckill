f2b4fcd994f87f9566448ae1989d470c2d7afb5a
Zhenglai Zhang <zhenglaizhang@hotmail.com>
2019-03-31 14:32:54 +0800

    enable hystrix for user service

--- user/src/main/java/org/ms/user/UserApplication.java
+++ user/src/main/java/org/ms/user/UserApplication.java
@@ -3,8 +3,10 @@ package org.ms.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
 @EnableEurekaClient
+@EnableHystrix
 @SpringBootApplication
 public class UserApplication {
 
